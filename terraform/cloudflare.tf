e992e0f97465c5a14fd4938ae291ccac38ce714f
Yevhen Shumilov <tazmans123@gmail.com>
2026-08-21 09:34:55 +0100

    add mqtt dns

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -7,3 +7,13 @@ resource "cloudflare_dns_record" "smartcar" {
   ttl     = 1
   proxied = true
 }
+
+resource "cloudflare_dns_record" "mqtt" {
+  zone_id = var.cloudflare_zone_id
+
+  name    = "mqtt"
+  type    = "A"
+  content = aws_instance.control_plane.public_ip
+  ttl     = 1
+  proxied = false
+}
