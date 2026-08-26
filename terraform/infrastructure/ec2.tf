9f31199917e0abe2d58f75b5fa26cdd5207992ca
Yevhen Shumilov <tazmans123@gmail.com>
2026-08-26 15:53:47 +0100

    config traefik

--- terraform/infrastructure/ec2.tf
+++ terraform/infrastructure/ec2.tf
@@ -4,17 +4,22 @@ resource "aws_key_pair" "main" {
 }
 
 resource "aws_instance" "control_plane" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.control_plane_instance_type
-  key_name      = aws_key_pair.main.key_name
-
-  subnet_id = aws_subnet.control_plane.id
+  ami                  = data.aws_ami.ubuntu.id
+  instance_type        = var.control_plane_instance_type
+  key_name             = aws_key_pair.main.key_name
+  iam_instance_profile = aws_iam_instance_profile.cloudwatch_agent.name
+  subnet_id            = aws_subnet.control_plane.id
 
   vpc_security_group_ids = [
     aws_security_group.control_plane.id
   ]
 
   associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
 
   tags = {
     Name    = "smartcar-control-plane"
@@ -24,17 +29,22 @@ resource "aws_instance" "control_plane" {
 }
 
 resource "aws_instance" "worker" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.worker_instance_type
-  key_name      = aws_key_pair.main.key_name
-
-  subnet_id = aws_subnet.worker.id
+  ami                  = data.aws_ami.ubuntu.id
+  instance_type        = var.worker_instance_type
+  key_name             = aws_key_pair.main.key_name
+  iam_instance_profile = aws_iam_instance_profile.cloudwatch_agent.name
+  subnet_id            = aws_subnet.worker.id
 
   vpc_security_group_ids = [
     aws_security_group.worker.id
   ]
 
   associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
 
   tags = {
     Name    = "smartcar-worker"
